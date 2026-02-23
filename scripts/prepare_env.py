2de863f9e686315d90545f4e85aaec7b86cc2514
fanlumaster <1730976608@qq.com>
2026-02-23 14:14:37 +0800

    some config files

--- scripts/prepare_env.py
+++ scripts/prepare_env.py
@@ -19,9 +19,18 @@ vcpkg_include_path = normpath(
         Project_root_path,
         "build",
         "vcpkg_installed",
-        "x64-windows",
+        "x64-windows-ort-static",
         "include",
     )
+)
+miniaudio_include_path = normpath(
+    os.path.join(Project_root_path, "third_party", "miniaudio")
+)
+whisper_include_path = normpath(
+    os.path.join(Project_root_path, "third_party", "whisper.cpp", "include")
+)
+ggml_include_path = normpath(
+    os.path.join(Project_root_path, "third_party", "whisper.cpp", "ggml", "include")
 )
 
 utfcpp_path = normpath(os.path.join(Project_root_path, "utfcpp", "source"))
@@ -43,14 +52,17 @@ cmake_toolchain_file_path = normpath(
 
 #
 # project_root/.clangd
-# line 7, 8, 9, 10
+# line 8, 9, 10, 11, 12
 #
 dot_clangd_file = os.path.join(Project_root_path, "scripts", "config_files", ".clangd")
 dot_clangd_output_file = os.path.join(Project_root_path, ".clangd")
 with open(dot_clangd_file, "r", encoding="utf-8") as f:
     lines = f.readlines()
-lines[6] = f'      "-I{Project_src_path}",\n'
-lines[7] = f'      "-I{vcpkg_include_path}",\n'
+lines[7] = f'      "-I{Project_src_path}",\n'
+lines[8] = f'      "-I{vcpkg_include_path}",\n'
+lines[9] = f'      "-I{miniaudio_include_path}",\n'
+lines[10] = f'      "-I{whisper_include_path}",\n'
+lines[11] = f'      "-I{ggml_include_path}",\n'
 with open(dot_clangd_output_file, "w", encoding="utf-8") as f:
     f.writelines(lines)
 
@@ -68,7 +80,7 @@ with open(CMakeLists_output_file, "w", encoding="utf-8") as f:
 
 #
 # CMakePresets.json
-# lines 9 12
+# lines 9, 12, 22, 25
 #
 CMakePresets_file = os.path.join(
     Project_root_path, "scripts", "config_files", "CMakePresets.json"
@@ -78,5 +90,7 @@ with open(CMakePresets_file, "r", encoding="utf-8") as f:
     lines = f.readlines()
 lines[8] = f'        "VCPKG_ROOT": "{vcpkg_root_path}"\n'
 lines[11] = f'        "CMAKE_TOOLCHAIN_FILE": "{cmake_toolchain_file_path}",\n'
+lines[21] = f'        "VCPKG_ROOT": "{vcpkg_root_path}"\n'
+lines[24] = f'        "CMAKE_TOOLCHAIN_FILE": "{cmake_toolchain_file_path}",\n'
 with open(CMakePresets_file_output_file, "w", encoding="utf-8") as f:
     f.writelines(lines)
